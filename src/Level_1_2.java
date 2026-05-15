013c6d4bf00ac6b308542bf1e2a6bc9806ca0688
tiendungcln <tiendung0301cln@gmail.com>
2026-05-15 17:19:16 +0700

    add Level_1_2

---
+++ src/Level_1_2.java
@@ -0,0 +1,31 @@
+public class Level_1_2 {
+    // Đếm bao nhiêu số 8 trong mảng
+    // Ví dụ:
+    // [8, 2, 8, 5, 8]
+    // x = 8
+    // => 3
+
+    public static int countOccurrences(int[] arr, int x){
+
+        int count = 0;
+
+        for (int num : arr){
+            if (num == x){
+                count ++;
+            }
+        }
+
+        return count;
+
+    }
+
+    public static void main(String[] args){
+
+        int[] arr = {8, 2, 8, 5, 8};
+        int x = 8;
+
+        System.out.println("Số lần " + x + " Xuất hiện trong mảng là: " +countOccurrences(arr, x));
+
+    }
+
+}
