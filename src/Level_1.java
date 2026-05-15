2599858d9ae48c645aff0f124d0ea42e40db503a
tiendungcln <tiendung0301cln@gmail.com>
2026-05-15 15:26:08 +0700

    add LinearSearch

---
+++ src/Level_1.java
@@ -0,0 +1,11 @@
+public class Level_1 {
+    // Tìm xem số x có trong mảng không
+    // [3, 7, 2, 9]
+    // x = 2
+    // => true
+
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/Level_1_1.java
@@ -0,0 +1,10 @@
+public class Level_1_1 {
+    // Trả về index đầu tiên của x
+    // [5, 8, 1, 8]
+    // x = 8
+    // => 1
+    public static void main(String[] args){
+
+    }
+
+}
