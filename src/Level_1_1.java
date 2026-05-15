9b286b306573c6f55833c78b7a2bab3ea82872e8
tiendungcln <tiendung0301cln@gmail.com>
2026-05-15 16:17:28 +0700

    add Level_1_1

--- src/Level_1_1.java
+++ src/Level_1_1.java
@@ -3,7 +3,25 @@ public class Level_1_1 {
     // [5, 8, 1, 8]
     // x = 8
     // => 1
+
+    public static int search_index(int[] arr, int x){
+
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == x){
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+
     public static void main(String[] args){
+
+        int[] arr = {5, 8, 1, 8};
+        int x = 8;
+
+        System.out.println("Index = " + search_index(arr, x));
 
     }
 
