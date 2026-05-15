42ff41b12da5dc07646a9370c520787aec0b1dff
tiendungcln <tiendung0301cln@gmail.com>
2026-05-15 16:11:28 +0700

    add Level_1

--- src/Level_1.java
+++ src/Level_1.java
@@ -4,8 +4,30 @@ public class Level_1 {
     // x = 2
     // => true
 
+    public static boolean search_x(int[] arr, int x){
+
+        for (int num : arr){
+            if (num == x){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
     public static void main(String[] args){
+
+        int[] arr = {3, 7, 2, 9};
+        int x = 2;
+
+        if (search_x(arr, x)){
+            System.out.println("Đã tìm thấy x");
+        }else{
+            System.out.println("Không tìm thấy x");
+        }
 
     }
 
 }
+
